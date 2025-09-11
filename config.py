8166cc13e72497457c3623e20426953cb145f07d
Weslley William Silva Pereira <weswil@hotmail.com>
2025-09-11 11:30:36 -0300

    lancamento do novo bot

--- config.py
+++ config.py
@@ -8,17 +8,12 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CANAL_ID_2 = os.getenv("CANAL_ID_2")
 ADMIN_USER_IDS = os.getenv("ADMIN_USER_IDS")
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 
 if not BOT_TOKEN:
     raise ValueError("CONFIG ERROR: BOT_TOKEN is not defined in environment variables or .env file.")
 
 if not CANAL_ID_2:
     raise ValueError("CONFIG ERROR: CANAL_ID_2 is not defined in environment variables or .env file.")
-
-if not DATABASE_URL:
-    raise ValueError("CONFIG ERROR: DATABASE_URL is not defined in environment variables or .env file.")
 
 try:
     CANAL_ID_2 = int(CANAL_ID_2)
